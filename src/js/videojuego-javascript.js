4e3d939a19c51488241ef2c8fe3f7951f8383ce2
Miguel Arlandy <marlandy@autentia.com>
2012-04-05 17:14:35 +0200

    hoja de estilos inicial

---
+++ src/js/videojuego-javascript.js
@@ -0,0 +1,590 @@
+window.requestAnimFrame = (function () {
+    return  window.requestAnimationFrame        ||
+        window.webkitRequestAnimationFrame  ||
+        window.mozRequestAnimationFrame     ||
+        window.oRequestAnimationFrame       ||
+        window.msRequestAnimationFrame      ||
+        function ( /* function */ callback, /* DOMElement */ element) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+arrayRemove = function (array, from) {
+    var rest = array.slice((from) + 1 || array.length);
+    array.length = from < 0 ? array.length + from : from;
+    return array.push.apply(array, rest);
+};
+
+var game = (function () {
+
+    // Variables globales a la aplicacion
+    var canvas,
+        ctx,
+        buffer,
+        bufferctx,
+        player,
+        evil,
+        playerShot,
+        bgMain,
+        bgBoss,
+        evilSpeed = 1,
+        totalEvils = 7,
+        playerLife = 3,
+        shotSpeed = 5,
+        evilCounter = 0,
+        youLoose = false,
+        congratulations = false,
+        minHorizontalOffset = 100,
+        maxHorizontalOffset = 400,
+        evilShots = 5,   // disparos que tiene el malo al principio
+        evilLife = 5,    // vidas que tiene el malo al principio
+        finalBossShots = 30,
+        finalBossLife = 20,
+        totalBestScoresToShow = 5, // las mejores puntuaciones que se mostraran
+        playerShotsBuffer = [],
+        evilShotsBuffer = [],
+        keyPressed = {},
+        keyMap = {
+            left: 37,
+            right: 39,
+            fire: 32     // tecla espacio
+        },
+        nextPlayerShot = 0,
+        playerShotDelay = 250,
+        now = 0;
+
+    function loop() {
+        update();
+        draw();
+    }
+
+    function init() {
+
+        showBestScores();
+
+        canvas = document.getElementById('canvas');
+        ctx = canvas.getContext("2d");
+
+        buffer = document.createElement('canvas');
+        buffer.width = canvas.width;
+        buffer.height = canvas.height;
+        bufferctx = buffer.getContext('2d');
+
+        bgMain = new Image();
+        bgMain.src = 'images/fondovertical.png';
+
+        bgBoss = new Image();
+        bgBoss.src = 'images/fondovertical_jefe.png';
+
+        player = new Player(playerLife, 0);
+        evilCounter = 1;
+        createNewEvil();
+
+        showLifeAndScore();
+
+        addListener(document, 'keydown', keyDown);
+        addListener(document, 'keyup', keyUp);
+
+        function anim () {
+            loop();
+            requestAnimFrame(anim);
+        }
+        anim();
+    }
+
+    function showLifeAndScore () {
+        bufferctx.fillStyle="rgb(59,59,59)";
+        bufferctx.font="bold 16px Arial";
+        bufferctx.fillText("Puntos: " + player.score, canvas.width - 100, 20);
+        bufferctx.fillText("Vidas: " + player.life, canvas.width - 100,40);
+    }
+
+    function getRandomNumber(range) {
+        return Math.floor(Math.random() * range);
+    }
+
+    function Player(life, score) {
+        var settings = {
+            bottomMargin : 10
+        };
+        player = new Image();
+        player.src = 'images/bueno.png';
+        player.posX = (canvas.width / 2) - (player.width / 2);
+        player.posY = canvas.height - (player.height == 0 ? 66 : player.height) - settings.bottomMargin;
+        player.life = life;
+        player.score = score;
+        player.dead = false;
+
+        player.speed = 5;
+
+        player.shoot = function () {
+            if (nextPlayerShot < now || now == 0) {
+                playerShot = new PlayerShot(player.posX + (player.width / 2) - 5 , player.posY);
+                playerShot.add();
+                now += playerShotDelay;
+                nextPlayerShot = now + playerShotDelay;
+            } else {
+                now = new Date().getTime();
+            }
+        };
+        return player;
+    }
+
+
+    /******************************* DISPAROS *******************************/
+    function Shot( x, y, array) {
+        this.posX = x;
+        this.posY = y;
+        this.image = new Image();
+        this.speed = shotSpeed;
+        this.identifier = 0;
+        this.add = function () {
+            array.push(this);
+        };
+        this.deleteShot = function (idendificador) {
+            arrayRemove(array, idendificador);
+        };
+    }
+
+    function PlayerShot (x, y) {
+        Object.getPrototypeOf(PlayerShot.prototype).constructor.call(this, x, y, playerShotsBuffer);
+        this.image.src =  'images/disparo_bueno.png';
+    }
+
+    PlayerShot.prototype = Object.create(Shot.prototype);
+    PlayerShot.prototype.constructor = PlayerShot;
+
+    function EvilShot (x, y) {
+        Object.getPrototypeOf(EvilShot.prototype).constructor.call(this, x, y, evilShotsBuffer);
+        this.image.src =  'images/disparo_malo.png';
+    }
+
+    EvilShot.prototype = Object.create(Shot.prototype);
+    EvilShot.prototype.constructor = EvilShot;
+    /******************************* FIN DISPAROS ********************************/
+
+
+    /******************************* ENEMIGOS *******************************/
+    function Enemy(life, shots) {
+        this.image = new Image();
+        this.imageNumber = 1;
+        this.animation = 0;
+        this.posX = getRandomNumber(canvas.width - this.image.width);
+        this.posY = -50;
+        this.life = life ? life : evilLife;
+        this.speed = evilSpeed;
+        this.shots = shots ? shots : evilShots;
+        this.dead = false;
+
+        var desplazamientoHorizontal = minHorizontalOffset +
+            getRandomNumber(maxHorizontalOffset - minHorizontalOffset);
+        this.minX = getRandomNumber(canvas.width - desplazamientoHorizontal);
+        this.maxX = this.minX + desplazamientoHorizontal - 40;
+        this.direction = 'D';
+
+
+        this.kill = function() {
+            this.dead = true;
+            totalEvils --;
+            this.image.src = this.image.src.replace(/\d/, '_muerto');
+            verifyToCreateNewEvil();
+        };
+
+        this.update = function () {
+            this.posY += this.goDownSpeed;
+            if (this.direction === 'D') {
+                if (this.posX <= this.maxX) {
+                    this.posX += this.speed;
+                } else {
+                    this.direction = 'I';
+                    this.posX -= this.speed;
+                }
+            } else {
+                if (this.posX >= this.minX) {
+                    this.posX -= this.speed;
+                } else {
+                    this.direction = 'D';
+                    this.posX += this.speed;
+                }
+            }
+            this.animation++;
+            if (this.animation > 5) {
+                this.animation = 0;
+                this.imageNumber ++;
+                if (this.imageNumber > 8) {
+                    this.imageNumber = 1;
+                }
+                this.image.src = this.image.src.replace(/\d/, this.imageNumber);
+            }
+        };
+
+        this.isOutOfScreen = function() {
+            return this.posY > (canvas.height + 15);
+        };
+
+        function shoot() {
+            if (evil.shots > 0 && !evil.dead) {
+                var disparo = new EvilShot(evil.posX + (evil.image.width / 2) - 5 , evil.posY + evil.image.height);
+                disparo.add();
+                evil.shots --;
+                setTimeout(function() {
+                    shoot();
+                }, getRandomNumber(3000));
+            }
+        }
+        setTimeout(function() {
+            shoot();
+        }, 1000 + getRandomNumber(2500));
+
+        this.toString = function () {
+            return 'Enemigo con vidas:' + this.life + 'shotss: ' + this.shots + ' puntos por matar: ' + this.pointsToKill;
+        }
+
+    }
+
+    function Evil (vidas, disparos) {
+        Object.getPrototypeOf(Evil.prototype).constructor.call(this, vidas, disparos);
+        this.image.src =  'images/malo1.png';
+        this.goDownSpeed = evilSpeed;
+        this.pointsToKill = 5 + evilCounter;
+    }
+
+    Evil.prototype = Object.create(Enemy.prototype);
+    Evil.prototype.constructor = Evil;
+
+    function FinalBoss () {
+        Object.getPrototypeOf(FinalBoss.prototype).constructor.call(this, finalBossLife, finalBossShots);
+        this.image.src =  'images/jefe1.png';
+        this.goDownSpeed = evilSpeed/2;
+        this.pointsToKill = 20;
+    }
+
+    FinalBoss.prototype = Object.create(Enemy.prototype);
+    FinalBoss.prototype.constructor = FinalBoss;
+    /******************************* FIN ENEMIGOS *******************************/
+
+    function verifyToCreateNewEvil() {
+        if (totalEvils > 0) {
+            setTimeout(function() {
+                createNewEvil();
+                evilCounter ++;
+            }, getRandomNumber(3000));
+
+        } else {
+            setTimeout(function() {
+                saveFinalScore();
+                congratulations = true;
+            }, 2000);
+
+        }
+    }
+
+    function createNewEvil() {
+        if (totalEvils != 1) {
+            evil = new Evil(evilLife + evilCounter - 1, evilShots + evilCounter - 1);
+        } else {
+            evil = new FinalBoss();
+        }
+    }
+
+    function isEvilHittingPlayer() {
+        return ( ( (evil.posY + evil.image.height) > player.posY && (player.posY + player.height) >= evil.posY ) &&
+            ((player.posX >= evil.posX && player.posX <= (evil.posX + evil.image.width)) ||
+                (player.posX + player.width >= evil.posX && (player.posX + player.width) <= (evil.posX + evil.image.width))));
+    }
+
+    function isShotHittingPlayer(evilShot) {
+        return (evilShot.posX >= player.posX && evilShot.posX <= (player.posX + player.width)
+            && evilShot.posY >= player.posY && evilShot.posY <= (player.posY + player.height));
+    }
+
+    function checkCollisions(shot) {
+        if (!evil.dead && shot.posX >= evil.posX && shot.posX <= (evil.posX + evil.image.width)) {
+            if (shot.posY >= evil.posY && shot.posY <= (evil.posY + evil.image.height)) {
+                if (evil.life > 1) {
+                    evil.life--;
+                } else {
+                    evil.kill();
+                    player.score += evil.pointsToKill;
+                }
+                shot.deleteShot(parseInt(shot.identifier));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function playerAction() {
+        if (player.dead)
+            return;
+        if (keyPressed.left && player.posX > 5)
+            player.posX -= player.speed;
+        if (keyPressed.right && player.posX < (canvas.width - player.width - 5))
+            player.posX += player.speed;
+        if (keyPressed.fire)
+            player.shoot();
+    }
+
+    function addListener(element, type, expression, bubbling) {
+        bubbling = bubbling || false;
+
+        if (window.addEventListener) { // Standard
+            element.addEventListener(type, expression, bubbling);
+        } else if (window.attachEvent) { // IE
+            element.attachEvent('on' + type, expression);
+        }
+    }
+
+    function keyDown(e) {
+        var key = (window.event ? e.keyCode : e.which);
+        for (var inkey in keyMap) {
+            if (key === keyMap[inkey]) {
+                e.preventDefault();
+                keyPressed[inkey] = true;
+            }
+        }
+    }
+
+    function keyUp(e) {
+        var key = (window.event ? e.keyCode : e.which);
+        for (var inkey in keyMap) {
+            if (key === keyMap[inkey]) {
+                e.preventDefault();
+                keyPressed[inkey] = false;
+            }
+        }
+    }
+
+    function draw() {
+        ctx.drawImage(buffer, 0, 0);
+    }
+
+    function showGameOver() {
+        bufferctx.fillStyle="rgb(255,0,0)";
+        bufferctx.font="bold 35px Arial";
+        bufferctx.fillText("GAME OVER", canvas.width / 2 - 100, canvas.height / 2);
+    }
+
+    function showCongratulations () {
+        bufferctx.fillStyle="rgb(204,50,153)";
+        bufferctx.font="bold 22px Arial";
+        bufferctx.fillText("Enhorabuena, te has pasado el juego!", canvas.width / 2 - 200, canvas.height / 2 - 30);
+        bufferctx.fillText("PUNTOS: " + player.score, canvas.width / 2 - 200, canvas.height / 2);
+        bufferctx.fillText("VIDAS: " + player.life + " x 5", canvas.width / 2 - 200, canvas.height / 2 + 30);
+        bufferctx.fillText("PUNTUACION TOTAL: " + getTotalScore(), canvas.width / 2 - 200, canvas.height / 2 + 60);
+    }
+
+    function getTotalScore() {
+        return player.score + player.life * 5;
+    }
+
+    function killPlayer() {
+        if (player.life > 0) {
+            player.dead = true;
+            evilShotsBuffer.splice(0, evilShotsBuffer.length);
+            playerShotsBuffer.splice(0, playerShotsBuffer.length);
+            player.src = 'images/bueno_muerto.png';
+            createNewEvil();
+            setTimeout(function () {
+                player = new Player(player.life - 1, player.score);
+            }, 500);
+
+        } else {
+            saveFinalScore();
+            youLoose = true;
+        }
+    }
+
+    function update() {
+
+        drawBackground();
+
+        if (congratulations) {
+            showCongratulations();
+            return;
+        }
+
+        if (youLoose) {
+            showGameOver();
+            return;
+        }
+
+        updateEvil();
+
+        bufferctx.drawImage(player, player.posX, player.posY);
+        bufferctx.drawImage(evil.image, evil.posX, evil.posY);
+
+        for (var j = 0; j < playerShotsBuffer.length; j++) {
+            var disparoBueno = playerShotsBuffer[j];
+            updatePlayerShot(disparoBueno, j);
+        }
+
+        if (isEvilHittingPlayer()) {
+            killPlayer();
+        } else {
+            for (var i = 0; i < evilShotsBuffer.length; i++) {
+                var evilShot = evilShotsBuffer[i];
+                updateEvilShot(evilShot, i);
+            }
+        }
+
+        showLifeAndScore();
+
+        playerAction();
+    }
+
+    function updatePlayerShot(playerShot, id) {
+        if (playerShot) {
+            playerShot.identifier = id;
+            if (checkCollisions(playerShot)) {
+                if (playerShot.posY > 0) {
+                    playerShot.posY -= playerShot.speed;
+                    bufferctx.drawImage(playerShot.image, playerShot.posX, playerShot.posY);
+                } else {
+                    playerShot.deleteShot(parseInt(playerShot.identifier));
+                }
+            }
+        }
+    }
+
+    function updateEvilShot(evilShot, id) {
+        if (evilShot) {
+            evilShot.identifier = id;
+            if (!isShotHittingPlayer(evilShot)) {
+                if (evilShot.posY <= canvas.height) {
+                    evilShot.posY += evilShot.speed;
+                    bufferctx.drawImage(evilShot.image, evilShot.posX, evilShot.posY);
+                } else {
+                    evilShot.deleteShot(parseInt(evilShot.identifier));
+                }
+            } else {
+                killPlayer();
+            }
+        }
+    }
+
+    function drawBackground() {
+        var background;
+        if (evil instanceof FinalBoss) {
+            background = bgBoss;
+        } else {
+            background = bgMain;
+        }
+        bufferctx.drawImage(background, 0, 0);
+    }
+
+    function updateEvil() {
+        if (!evil.dead) {
+            evil.update();
+            if (evil.isOutOfScreen()) {
+                evil.kill();
+            }
+        }
+    }
+
+    /******************************* MEJORES PUNTUACIONES (LOCALSTORAGE) *******************************/
+    function saveFinalScore() {
+        localStorage.setItem(getFinalScoreDate(), getTotalScore());
+        showBestScores();
+        removeNoBestScores();
+    }
+
+    function getFinalScoreDate() {
+        var date = new Date();
+        return fillZero(date.getDay()+1)+'/'+
+            fillZero(date.getMonth()+1)+'/'+
+            date.getFullYear()+' '+
+            fillZero(date.getHours())+':'+
+            fillZero(date.getMinutes())+':'+
+            fillZero(date.getSeconds());
+    }
+
+    function fillZero(number) {
+        if (number < 10) {
+            return '0' + number;
+        }
+        return number;
+    }
+
+    function getBestScoreKeys() {
+        var bestScores = getAllScores();
+        bestScores.sort(function (a, b) {return b - a;});
+        bestScores = bestScores.slice(0, totalBestScoresToShow);
+        var bestScoreKeys = [];
+        for (var j = 0; j < bestScores.length; j++) {
+            var score = bestScores[j];
+            for (var i = 0; i < localStorage.length; i++) {
+                var key = localStorage.key(i);
+                if (parseInt(localStorage.getItem(key)) == score) {
+                    bestScoreKeys.push(key);
+                }
+            }
+        }
+        return bestScoreKeys.slice(0, totalBestScoresToShow);
+    }
+
+    function getAllScores() {
+        var all = [];
+        for (var i=0; i < localStorage.length; i++) {
+            all[i] = (localStorage.getItem(localStorage.key(i)));
+        }
+        return all;
+    }
+
+    function showBestScores() {
+        var bestScores = getBestScoreKeys();
+        var bestScoresList = document.getElementById('puntuaciones');
+        if (bestScoresList) {
+            clearList(bestScoresList);
+            for (var i=0; i < bestScores.length; i++) {
+                addListElement(bestScoresList, bestScores[i], i==0?'negrita':null);
+                addListElement(bestScoresList, localStorage.getItem(bestScores[i]), i==0?'negrita':null);
+            }
+        }
+    }
+
+    function clearList(list) {
+        list.innerHTML = '';
+        addListElement(list, "Fecha");
+        addListElement(list, "Puntos");
+    }
+
+    function addListElement(list, content, className) {
+        var element = document.createElement('li');
+        if (className) {
+            element.setAttribute("class", className);
+        }
+        element.innerHTML = content;
+        list.appendChild(element);
+    }
+
+    // extendemos el objeto array con un metodo "contiene"
+    Array.prototype.containsElement = function(element) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == element) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    function removeNoBestScores() {
+        var scoresToRemove = [];
+        var bestScoreKeys = getBestScoreKeys();
+        for (var i=0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            if (!bestScoreKeys.containsElement(key)) {
+                scoresToRemove.push(key);
+            }
+        }
+        for (var j = 0; j < scoresToRemove.length; j++) {
+            var scoreToRemoveKey = scoresToRemove[j];
+            localStorage.removeItem(scoreToRemoveKey);
+        }
+    }
+    /******************************* FIN MEJORES PUNTUACIONES *******************************/
+
+    return {
+        init: init
+    }
+
+})();
